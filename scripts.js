ef72f66d5a02abe84fce3271b0a383a5fb0a3a64
Caeli1010 <elizabeth.ayala4@gmail.com>
2021-05-23 23:28:29 -0300

    UpDate

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@ function foto(){
     var anios= prompt("¿cuántos años tenes?");
     alert("Me decis que tu edad es: " + anios + " años");
         }
+//         document.getElementById("Proyectos")
+// function imagen(){
+//     const nombreImg= {0:"foto1.jpg", 1:"foto2.jpg", 2:"foto3.jpg"};
+//     let cssProyectos;
+//     let i=0;
+//     for(i;i<3 ;i++){
+//         cssProyectos="width: 300px; min-height: 350px; background: url(img/"+nombreImg[i]+") center center no-repeat;background-size: cover;"
+//         // console.log(nombreImg[i]+ " " );
+//     }
+// }
