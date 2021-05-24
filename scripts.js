08ea333b5b1fcd2c68b934bca6a77e961071313f
Caeli1010 <elizabeth.ayala4@gmail.com>
2021-05-23 21:04:07 -0300

    UpDate

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function foto(){
+    var edad=31;
+    alert("¡Hola! Soy Elizabeth, tengo "+ edad + " años");
+    var anios= prompt("¿cuántos años tenes?");
+    alert("Me decis que tu edad es: " + anios + " años");
+        }
